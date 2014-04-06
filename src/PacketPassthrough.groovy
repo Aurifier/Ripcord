49118bb84b664ef703074cc556b8596946d4e37d
Aurifier <reed.995@osu.edu>
2014-04-06 00:43:59 -0400

    Quick attempt at a passthrough hack. Doesn't work and now I must go to bed. (btw:  to get a jar for now.)

---
+++ src/PacketPassthrough.groovy
@@ -0,0 +1,32 @@
+
+/**
+ * Created by Aurifier on 4/5/14.
+ */
+def mcHost = "play.thatonetechserver.com"
+def mcPort = 25565
+def listenSocket = new ServerSocket(25565)
+
+while(true) {
+    listenSocket.accept { clientSock ->
+        println("Client connected.")
+        def mcSocket = new Socket(mcHost, mcPort)
+        mcSocket.withStreams { mcInputStream, mcOutputStream ->
+            def mcReader = mcInputStream.newReader()
+            def mcWriter = mcOutputStream.newWriter()
+            clientSock.withStreams { clientInputStream, clientOutputStream ->
+                def clientReader = clientInputStream.newReader()
+                def clientWriter = clientOutputStream.newWriter()
+                while(!clientSock.isClosed()) {
+                    if(clientReader.ready()) {
+                        mcWriter.write(clientReader.read())
+                    }
+
+                    if(mcReader.ready()) {
+                        clientWriter.write(mcReader.read())
+                    }
+                }
+            }
+            mcSocket.close()
+        }
+    }
+}
