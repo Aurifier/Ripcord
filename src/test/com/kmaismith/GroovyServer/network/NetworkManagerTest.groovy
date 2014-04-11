8693733a49259e5196092efbab2855ff9e8cce82
Kyle Smith <kmaismith@gmail.com>
2014-04-10 21:38:35 -0400

    gradleized

---
+++ src/test/com/kmaismith/GroovyServer/network/NetworkManagerTest.groovy
@@ -0,0 +1,16 @@
+package com.kmaismith.GroovyServer.network
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: kyle
+ * Date: 3/21/14
+ * Time: 11:53 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class NetworkManagerTest extends GroovyTestCase {
+    def testSomething() {
+        //NetworkManager netMgr = new NetworkManager()
+
+        //netMgr.doNetworkStuff()
+    }
+}
